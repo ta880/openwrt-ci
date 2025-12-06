d51be3abd29fd24dd902d13b01294f7f107c7825
ta880 <taoxu1274375451@gmail.com>
2025-12-06 09:01:47 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -58,6 +58,7 @@ UPDATE_PACKAGE() {
 #UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-24.10"
 UPDATE_PACKAGE "luci-theme-argon" "kiddin9/kwrt-packages" "main" "pkg"
 UPDATE_PACKAGE "theme-kucat" "sirpdboy/luci-theme-kucat" "master"
+UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 
 UPDATE_PACKAGE "luci-app-kucat" "sirpdboy/luci-app-kucat-config"
 UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
